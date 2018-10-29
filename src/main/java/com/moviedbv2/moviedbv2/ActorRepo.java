e2b9a97ea6a37ad7f481c3732a586ff71c6b8a9a
niklasld <niklasld@gmail.com>
2018-10-29 11:20:58 +0100

    blah

--- src/main/java/com/moviedbv2/moviedbv2/ActorRepo.java
+++ src/main/java/com/moviedbv2/moviedbv2/ActorRepo.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
